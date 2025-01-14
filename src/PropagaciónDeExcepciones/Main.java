c25d61dc41a61bd1cbbf6efd0231f69fb0b072f0
Antonio Carrasco <carrasco.antonio.233@gmail.com>
2025-01-14 12:19:00 +0100

    cambios

--- src/PropagaciónDeExcepciones/Main.java
+++ src/PropagaciónDeExcepciones/Main.java
@@ -1,4 +1,24 @@
 package PropagaciónDeExcepciones;
 
 public class Main {
+    public static void main(String[] args) {
+        try {
+            metodo1();
+        } catch (ArithmeticException e) {
+            System.out.println("Capture exception " + e.getMessage());
+        }
+    }
+
+    static void metodo1() {
+        metodo2();
+    }
+
+    static void metodo2() {
+        metodo3();
+    }
+
+    static void metodo3() {
+        int resultado = 10 / 0;  // Esto lanza ArithmeticException
+    }
 }
+
